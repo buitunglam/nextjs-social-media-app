fd3863183e8672dfc765c82860f021bb1d6655a2
buitunglam <tunglam.xm94@gmail.com>
2025-07-09 22:39:30 +0700

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // config for time cache data. it reload every 30 seconds
+  experimental: {
+    staleTimes: {
+      dynamic: 30,
+
+    }
+  }
 };
 
 export default nextConfig;
